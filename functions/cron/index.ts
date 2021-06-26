74ce1cecfe2648bdf2666a49ba021402a774c82d
Nathan Arthur <nathan@nathanarthur.com>
2021-06-26 15:25:09 -0700

    testing func

--- functions/cron/index.ts
+++ functions/cron/index.ts
@@ -2,6 +2,8 @@ import * as functions from "firebase-functions";
 import cron from "./cron";
 // import * as admin from "firebase-admin"
 
-export const dialFunc = functions.https.onRequest(() => {
-  cron();
-});
+export const handler = async (): Promise<string> => {
+  // await cron();
+  return "cron success";
+};
+
