383c2b547af311e47cf6661bb99bf2ee03b362b7
Nathan Arthur <nathan@nathanarthur.com>
2021-06-26 16:11:15 -0700

    try to init firestore in prod

--- functions/cron/index.ts
+++ functions/cron/index.ts
@@ -1,10 +1,8 @@
-// import * as functions from "firebase-functions";
-// import doCron from "./doCron";
+import doCron from "./doCron";
 import {Handler} from "@netlify/functions";
-// import * as admin from "firebase-admin"
 
 const handler: Handler = async () => {
-  // await doCron();
+  await doCron();
   return {
     statusCode: 200,
     body: "cron success",
