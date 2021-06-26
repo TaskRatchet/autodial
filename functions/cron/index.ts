1feb38cde92fcfb768060c3739ccee6118d19d7c
Nathan Arthur <nathan@nathanarthur.com>
2021-06-26 15:38:15 -0700

    add function types

--- functions/cron/index.ts
+++ functions/cron/index.ts
@@ -1,8 +1,9 @@
 import * as functions from "firebase-functions";
 import cron from "./cron";
+import {Handler} from "@netlify/functions";
 // import * as admin from "firebase-admin"
 
-const handler = async (): Promise<{statusCode: number, body: string}> => {
+const handler: Handler = async () => {
   // await cron();
   return {
     statusCode: 200,
