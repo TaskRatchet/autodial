672d62c2eefeda707e7c6726dc8cbad9be565a20
Nathan Arthur <nathan@nathanarthur.com>
2022-01-25 11:43:57 -0500

    try to fix cloud function

--- functions/src/database.ts
+++ functions/src/database.ts
@@ -1,30 +1,38 @@
-import admin from "firebase-admin";
-import {firestore} from "firebase-admin/lib/firestore";
-import Firestore = firestore.Firestore;
-import QueryDocumentSnapshot = firestore.QueryDocumentSnapshot;
-import DocumentData = firestore.DocumentData;
+import {Firestore, QueryDocumentSnapshot} from "@google-cloud/firestore";
+// import admin from "firebase-admin";
+// import {firestore} from "firebase-admin/lib/firestore";
+// import Firestore = firestore.Firestore;
+// import QueryDocumentSnapshot = firestore.QueryDocumentSnapshot;
+// import DocumentData = firestore.DocumentData;
 import {User} from "../../src/lib";
 
-let _db: Firestore;
+const firestore = new Firestore({
+  projectId: "autodial-dfeb8",
+  timestampsInSnapshots: true,
+});
 
-function getDb() {
-  if (!_db) {
-    // admin.initializeApp({
-    //   credential: admin.credential.cert({
-    //     projectId: "autodial-dfeb8",
-    //     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    //     // Need to un-escape linebreaks from private key.
-    //     // See: https://stackoverflow.com/a/50376092/6451879
-    //     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    //   }),
-    // });
-    _db = admin.firestore();
-  }
 
-  return _db;
-}
-
+//
+// let _db: Firestore;
+//
+// function getDb() {
+//   if (!_db) {
+//     admin.initializeApp({
+//       credential: admin.credential.cert({
+//         projectId: "autodial-dfeb8",
+//         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//         // Need to un-escape linebreaks from private key.
+//         // See: https://stackoverflow.com/a/50376092/6451879
+//         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+//       }),
+//     });
+//     _db = admin.firestore();
+//   }
+//
+//   return _db;
+// }
+//
 export async function getUsers(): Promise<User[]> {
-  const {docs} = await getDb().collection("users").get();
-  return docs.map((d: QueryDocumentSnapshot<DocumentData>) => d.data() as User);
+  const {docs} = await firestore.collection("users").get();
+  return docs.map((d: QueryDocumentSnapshot) => d.data() as User);
 }
