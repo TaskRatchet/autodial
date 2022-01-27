c8c62d4bf6f398d6c358406fd016aa7fcb41524b
Nathan Arthur <nathan@nathanarthur.com>
2022-01-27 13:57:53 -0500

    cleanup

--- functions/src/database.ts
+++ functions/src/database.ts
@@ -1,34 +1,9 @@
-// import functions from "firebase-functions";
 import admin, {firestore} from "firebase-admin";
-// import {firestore} from "firebase-admin/lib/firestore";
-// import Firestore = firestore.Firestore;
-// import QueryDocumentSnapshot = firestore.QueryDocumentSnapshot;
-// import DocumentData = firestore.DocumentData;
 import {User} from "../../src/lib";
 import QueryDocumentSnapshot = firestore.QueryDocumentSnapshot;
 
 admin.initializeApp();
 
-//
-// let _db: Firestore;
-//
-// function getDb() {
-//   if (!_db) {
-//     admin.initializeApp({
-//       credential: admin.credential.cert({
-//         projectId: "autodial-dfeb8",
-//         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//         // Need to un-escape linebreaks from private key.
-//         // See: https://stackoverflow.com/a/50376092/6451879
-//         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-//       }),
-//     });
-//     _db = admin.firestore();
-//   }
-//
-//   return _db;
-// }
-//
 export async function getUsers(): Promise<User[]> {
   const {docs} = await admin.firestore().collection("users").get();
   return docs.map((d: QueryDocumentSnapshot) => d.data() as User);
