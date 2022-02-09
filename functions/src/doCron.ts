9ce8c8e3c8c25ff757825e8c5de1cd215b00df3a
Nathan Arthur <nathan@nathanarthur.com>
2022-02-09 08:15:46 -0500

    handle get goals exceptions

--- functions/src/doCron.ts
+++ functions/src/doCron.ts
@@ -24,40 +24,43 @@ const doCron = async (): Promise<void> => {
       return;
     }
 
-    const all = await getGoals(beeminder_user, beeminder_token);
+    try {
+      const all = await getGoals(beeminder_user, beeminder_token);
+      const toDial = all.filter((g: Goal) => getSettings(g).autodial);
 
-    const toDial = all.filter((g: Goal) => getSettings(g).autodial);
-
-    await Promise.all(toDial.map(async (g) => {
-      log(`start dial goal ${beeminder_user}/${g.slug}`);
-      try {
-        const {min, max} = getSettings(g);
-        const fullGoal = await getGoal(
-            beeminder_user,
-            beeminder_token,
-            g.slug,
-            now() - (SID * 31),
-        );
-        const roadall = dial(fullGoal, {min, max});
-        const newRate = roadall && roadall[roadall.length - 1][2];
-        const id = `${beeminder_user}/${g.slug}`;
-
-        log(`end dial goal ${id}: ${newRate}`);
-
-        if (!roadall) return;
-
-        if (!isDev) {
-          await updateGoal(
+      await Promise.all(toDial.map(async (g) => {
+        log(`start dial goal ${beeminder_user}/${g.slug}`);
+        try {
+          const {min, max} = getSettings(g);
+          const fullGoal = await getGoal(
               beeminder_user,
               beeminder_token,
               g.slug,
-              {roadall}
+              now() - (SID * 31),
           );
+          const roadall = dial(fullGoal, {min, max});
+          const newRate = roadall && roadall[roadall.length - 1][2];
+          const id = `${beeminder_user}/${g.slug}`;
+
+          log(`end dial goal ${id}: ${newRate}`);
+
+          if (!roadall) return;
+
+          if (!isDev) {
+            await updateGoal(
+                beeminder_user,
+                beeminder_token,
+                g.slug,
+                {roadall}
+            );
+          }
+        } catch (e) {
+          log({m: "failed to dial goal", g, e});
         }
-      } catch (e) {
-        log({m: "failed to dial goal", g, e});
-      }
-    }));
+      }));
+    } catch (e) {
+      log({m: "failed to handle user", beeminder_user, e});
+    }
   }));
 };
 
