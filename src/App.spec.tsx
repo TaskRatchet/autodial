d2e2ccfc57faf5e347cdf796262c0bc60355f13f
Nathan Arthur <nathan@nathanarthur.com>
2022-03-04 08:57:01 -0500

    syntax error

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -541,6 +541,7 @@ describe("Home page", () => {
       expect(getGoalsVerbose).toBeCalledTimes(2);
     });
   });
+});
 
 // TODO:
 // add route to handle de-authorizing the app from Beeminder side (will need
